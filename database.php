f0795ce1a7816415825f4a236be0352556b52cad
marcobtech <marcobtechen@gmail.com>
2026-04-22 18:54:36 -0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = getenv("DB_HOST");
+$db   = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
