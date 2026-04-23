d908fb67447725e33c429a3aff43fcbebf11720e
marcobtech <marcobtechen@gmail.com>
2026-04-22 21:15:59 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,71 @@
+// const axios = require("axios");
+
+// class Api {
+//     constructor() {
+//         this.api_url = "https://painelpro.net/api/v2";
+//         this.api_key = process.env.PP_TOKEN;
+//     }
+
+//     async order(data) {
+//         const payload = {
+//             key: this.api_key,
+//             action: "add",
+//             ...data
+//         };
+
+//         try {
+//             const res = await axios.post(
+//                 this.api_url,
+//                 new URLSearchParams(payload),
+//                 {
+//                     timeout: 10000 // 🔥 evita ETIMEDOUT
+//                 }
+//             );
+
+//             return res.data;
+
+//         } catch (err) {
+//             console.log("API ORDER ERROR:", err.message);
+
+//             return {
+//                 error: true,
+//                 message: err.message
+//             };
+//         }
+//     }
+
+//     async status(order_id) {
+//         const payload = {
+//             key: this.api_key,
+//             action: "status",
+//             order: order_id
+//         };
+
+//         try {
+//             const res = await axios.post(
+//                 this.api_url,
+//                 new URLSearchParams(payload),
+//                 {
+//                     timeout: 10000
+//                 }
+//             );
+
+//             return res.data;
+
+//         } catch (err) {
+//             console.log("API STATUS ERROR:", err.message);
+
+//             return {
+//                 error: true,
+//                 message: err.message
+//             };
+//         }
+//     }
+// }
+
+// module.exports = Api;
+
+
 const axios = require("axios");
 
 class Api {
@@ -13,53 +81,13 @@ class Api {
             ...data
         };
 
-        try {
-            const res = await axios.post(
-                this.api_url,
-                new URLSearchParams(payload),
-                {
-                    timeout: 10000 // 🔥 evita ETIMEDOUT
-                }
-            );
+        const res = await axios.post(
+            this.api_url,
+            new URLSearchParams(payload),
+            { timeout: 10000 }
+        );
 
-            return res.data;
-
-        } catch (err) {
-            console.log("API ORDER ERROR:", err.message);
-
-            return {
-                error: true,
-                message: err.message
-            };
-        }
-    }
-
-    async status(order_id) {
-        const payload = {
-            key: this.api_key,
-            action: "status",
-            order: order_id
-        };
-
-        try {
-            const res = await axios.post(
-                this.api_url,
-                new URLSearchParams(payload),
-                {
-                    timeout: 10000
-                }
-            );
-
-            return res.data;
-
-        } catch (err) {
-            console.log("API STATUS ERROR:", err.message);
-
-            return {
-                error: true,
-                message: err.message
-            };
-        }
+        return res.data;
     }
 }
 
