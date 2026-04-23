a7555a46a1afaffc6447def48ff8ab6dd49422e6
marcobtech <marcobtechen@gmail.com>
2026-04-22 22:48:24 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -84,7 +84,7 @@ async function processOrders() {
             console.log("📡 Enviando para API externa...");
 
             const result = await api.order({
-                service: 1284,
+                service: 1289,
                 link: order.link,
                 quantity: order.quantity
             });
